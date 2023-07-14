9db2209a0db959a02fbad7be7bca7f36eb59bc7a
Lakshdeep Singh <lakshdeep.code@gmail.com>
2023-07-14 14:52:23 +0530

    Modified src/messages.py

--- src/messages.py
+++ src/messages.py
@@ -9,9 +9,14 @@ def display_menu():
     print("'q' For Exiting app.")
     print("Under Development....\n------------")
 
+def relaunch_prompt():
+    print("\nWould you like to go again (Y/N) ? : ")
+
 def exit_message():
     print("-------\nProgram Ended, Visit Again !\n-------")
     
+def password_len_prompt():
+    print("Enter the Password Length (atleast 8) : ",end="")
 
 def len_error_message():
     print("-------\nPassword length not within range(8,14).\n-------")
