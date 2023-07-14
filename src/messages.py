a31216182409334320a9bccce801c98071ec9333
Lakshdeep Singh <lakshdeep.code@gmail.com>
2023-07-14 15:37:00 +0530

    Modified and extended some functionality in messages.py

--- src/messages.py
+++ src/messages.py
@@ -1,22 +1,22 @@
 #Main Generator Message 
 def welcome_message():
     print("-------------------------------------\nWelcome ! To Password Generator..")
-    print("-------------------------------------\n")
+    print("-------------------------------------")
     
 def display_menu():
-    print("------------\nWhat would you like to do ?\n")
-    print("'1' For Creating a Password.")
+    print("\n-------\n'1' For Creating a Password.")
     print("'q' For Exiting app.")
     print("Under Development....\n------------")
+    print("What would you like to do ? : ", end="")
 
 def relaunch_prompt():
-    print("\nWould you like to go again (Y/N) ? : ")
+    print("\nWould you like to go again (Y/N) ? : ", end="")
 
 def exit_message():
     print("-------\nProgram Ended, Visit Again !\n-------")
     
 def password_len_prompt():
-    print("Enter the Password Length (atleast 8) : ",end="")
+    print("\nEnter the Password Length (atleast 8) : ",end="")
 
 def len_error_message():
     print("-------\nPassword length not within range(8,14).\n-------")
