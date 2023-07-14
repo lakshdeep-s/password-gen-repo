29c9ad71646fecffbe34b96e29a428c34ea1bb0e
Lakshdeep Singh <lakshdeep.code@gmail.com>
2023-07-14 14:38:45 +0530

    Added Function to random_case the password

--- src/utl_funcs.py
+++ src/utl_funcs.py
@@ -1,8 +1,21 @@
+import random
 def get_userchoice():
-    choice = input("Enter you choice : ")
+    choice = input()
     return choice
 
 def validate_pass_len(len):
     if len < 8 or len > 14:
         return -1
     return 1
+
+def random_case(string):
+    result = ""
+    for char in string:
+        if char.isalpha():
+            if random.choice([True, False]):
+                result += char.upper()
+            else:
+                result += char.lower()
+        else:
+            result += char
+    return result
