0a1c0b23dd46db450f3bc9787b5d68cdfd178b24
Lakshdeep Singh <lakshdeep.code@gmail.com>
2023-07-14 15:07:36 +0530

    Fixed Minor bug in generator.py (variable name)

--- src/generator.py
+++ src/generator.py
@@ -1,14 +1,15 @@
 from utl_funcs import *
+from messages import len_error_message, password_len_prompt
 import random, string
 
 def pass_generator():
     password = str()
     
-    print("Enter the Password Length (atleast 8) : ",end="")
-    len = int(get_userchoice())
-    can_proceed = validate_pass_len(len)
+    password_len_prompt()
+    length = int(get_userchoice())
+    can_proceed = validate_pass_len(length)
     
-    if can_proceed:
+    if can_proceed == 1:
         for i in range(2):
             password+=random.choice(string.punctuation)
         for i in range(4):
@@ -16,12 +17,15 @@ def pass_generator():
         #adding an uppercase letter
         password+=random.choice(string.ascii_uppercase)
         #filling rest with lcase letters
-        for i in range(len - 7):
+        for i in range(length - 7):
             password+=random.choice(string.ascii_letters)
     else :
-        pass
+        len_error_message()
+        return ""
     
     password = random_case(password)
-    
-    password = ''.join(random.sample(password, len))
+    password = ''.join(random.sample(password, length))
     return password
+
+res = pass_generator()
+print(res)
