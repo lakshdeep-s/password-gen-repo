fd2b35036648eabb872c3410a55b31de56c51684
Lakshdeep Singh <lakshdeep.code@gmail.com>
2023-07-14 13:36:59 +0530

    Modified src/messages.py

---
+++ src/messages.py
@@ -0,0 +1,9 @@
+#Main Generator Message 
+def welcome_message():
+    print("-------------------------------------\nWelcome ! To Password Generator..")
+    print("-------------------------------------\n")
+    
+def display_menu():
+    print("------------\nWhat would you like to do ?\n")
+    print("'1' For Creating a Password.")
+    print("Under Development....\n------------")
