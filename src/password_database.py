1d8faeef4e41a9ecea665d31a84482dbd544ea7f
Lakshdeep Singh <lakshdeep.code@gmail.com>
2023-07-15 10:15:25 +0530

    Modified src/password_database.py and added more functionality to Password class

--- src/password_database.py
+++ src/password_database.py
@@ -2,10 +2,20 @@ from collections import deque
 
 class Password:
     database = deque()
+    no_of_passwords = 0
     def __init__(self, password: str, time: str) -> None:
         self.password = password
         self.time_of_gen = time
-        self.database.append((time, password))    
+        self.database.append((time, password))
+        Password.no_of_passwords+=1
+        
+    def get_password(self):
+        return self.password
+        
+    @classmethod
+    def get_no_of_password(cls):
+        return cls.no_of_passwords
+        
     @classmethod
     def get_last_password(cls):
         if cls.database:
