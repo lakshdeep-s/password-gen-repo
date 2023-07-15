a14abb5718b69f913720a38de82bc880aac849bc
Lakshdeep Singh <lakshdeep.code@gmail.com>
2023-07-15 10:40:14 +0530

    Modified src/main.py , Added more features

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 from messages import *
 from utl_funcs import *
 from generator import *
+import time as ti
+from password_database import Password
 
 welcome_message()
 relaunch = 1
@@ -11,8 +13,21 @@ while relaunch:
     
     if choice == '1':
         password = pass_generator()
-        print('Your Password is : ', password)
-        
+        at_time = ti.time()
+        pass_obj = Password(password, at_time)
+        print('Your Password is : ', pass_obj.get_password())
+    
+    elif choice == '2':
+        Password.display_all_pass()
+    
+    elif choice == '3':
+        last_past = Password.get_last_password()
+        print("Last Password : ", last_past)
+    
+    elif choice == '4':
+        no_of_pass = Password.get_no_of_password()
+        print("Total Number of Passwords generated : ", no_of_pass)
+    
     elif choice.upper() == 'Q':
         break
     
